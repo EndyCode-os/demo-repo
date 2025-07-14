a5dbecdf5328949a475b288996dc63f4afe1951c
Endy Pongnon <endypongnon4@gmail.com>
2025-07-13 23:07:12 -0400

    update to the main branch

---
+++ main.py
@@ -0,0 +1,7 @@
+x = int(input('Enter your number'))
+if x < 10:
+    print("Number is inferior to ten")
+elif x==10:
+    print('Number is equal to ten')
+else:
+    print('Number is superior to ten')
